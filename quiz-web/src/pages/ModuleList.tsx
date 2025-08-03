f50f1f8ae4cf432b764df7106aa2242b9d50d580
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-08-03 17:30:22 +0530

    Update ModuleList.tsx

--- quiz-web/src/pages/ModuleList.tsx
+++ quiz-web/src/pages/ModuleList.tsx
@@ -117,7 +117,7 @@ const ModuleList: React.FC = () => {
                     </Link>
                     <Link 
                       to={`/module/${module.id}/quizzes`}
-                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700"
+                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-smartmind-dark hover:bg-smartmind-medium"
                     >
                       Quizzes
                     </Link>
