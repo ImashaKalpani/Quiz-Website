f360b5b8957a4cdf0298dd1b00b93455bcd4e6cf
ImashaKalpani <kalpaniimasha76@gmail.com>
2025-09-21 13:29:55 +0530

    Update QuizPage.tsx

--- quiz-web/src/pages/QuizPage.tsx
+++ quiz-web/src/pages/QuizPage.tsx
@@ -50,8 +50,8 @@ const QuizPage: React.FC = () => {
       <p className="mb-6">{quiz.description}</p>
 
       {quiz.questions.map((q, idx) => (
-        <div key={idx} className="mb-6 p-4 border rounded bg-white">
-          <p className="font-semibold mb-2">
+        <div key={idx} className="mb-10 p-5 border rounded bg-white border-black">
+          <p className="font-bold mb-4">
             {idx + 1}. {q.questionText}
           </p>
           <ul className="space-y-2">
@@ -65,10 +65,10 @@ const QuizPage: React.FC = () => {
                 <li
                   key={i}
                   onClick={() => !submitted && handleSelectAnswer(idx, opt)}
-                  className={`p-2 border rounded cursor-pointer hover:bg-gray-100
-                    ${isSelected ? "bg-blue-100 border-blue-400" : ""}
-                    ${isCorrect ? "bg-green-200 border-green-400" : ""}
-                    ${isWrong ? "bg-red-200 border-red-400" : ""}
+                  className={`p-2 border rounded cursor-pointer hover:bg-gray-100 border-black
+                    ${isSelected ? "border-black bg-blue-100 border-blue-400" : ""}
+                    ${isCorrect ? "border-black bg-green-200 border-green-400" : ""}
+                    ${isWrong ? "border-black bg-red-200 border-red-400" : ""}
                   `}
                 >
                   {opt}
